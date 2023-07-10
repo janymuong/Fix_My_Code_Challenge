efe6e5ba09aeb61cff6bf949c708829d76218fac
Jany Muong <me.roumuong@gmail.com>
2023-07-10 23:07:42 +0300

    use original str-format

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -5,31 +5,31 @@ I love geometry! - file for a square class
 
 
 class Square():
-    '''repr for a square class'''
+    """ Square class """
     width = 0
     height = 0
 
     def __init__(self, *args, **kwargs):
-        '''constructor to initialze attributes'''
+        """ Instantiation of class """
         for key, value in kwargs.items():
             setattr(self, key, value)
 
     def area_of_my_square(self):
-        '''area of the square'''
+        """ Area of the square """
         return self.width * self.height
 
-    def PermiterOfMySquare(self):
-        '''perimeter of a geometric square'''
+    def permiter_of_my_square(self):
+        """ Perimeter of my square """
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
-        '''return string representation of square'''
-        return '{}/{}'.format(self.width, self.height)
+        """ Printable representation """
+        return "{}/{}".format(self.width, self.height)
 
 
-if __name__ == '__main__':
-
+if __name__ == "__main__":
+    """ Create a square object """
     s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
-    print(s.PermiterOfMySquare())
+    print(s.permiter_of_my_square())
