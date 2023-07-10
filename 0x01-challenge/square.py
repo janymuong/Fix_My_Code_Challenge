ac3edfcca808a5d9d0cc92e29095fead5a508864
Jany Muong <me.roumuong@gmail.com>
2023-07-10 22:41:51 +0300

    fix code for geometric square methods

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,28 +1,33 @@
 #!/usr/bin/python3
+'''module:
+I love geometry! - file for a square class
+'''
 
-class square():
-    
-    width = 0
-    height = 0
 
-    
+class Square():
+    '''repr for a square class'''
+    size = 0
+
     def __init__(self, *args, **kwargs):
         for key, value in kwargs.items():
             setattr(self, key, value)
 
     def area_of_my_square(self):
-        """ Area of the square """
-        return self.width * self.width
+        '''area of the square'''
+        return self.size ** 2
 
     def PermiterOfMySquare(self):
-        return (self.width * 2) + (self.height * 2)
+        '''perimeter of a geometric square'''
+        return (self.size * 4)
 
     def __str__(self):
-        return "{}/{}".format(self.width, self.height)
+        '''return string representation of square'''
+        return f'{self.size}/{self.size}'
 
-if __name__ == "__main__":
 
-    s = square(width=12, height=9)
+if __name__ == '__main__':
+
+    s = Square(size=12)
     print(s)
     print(s.area_of_my_square())
     print(s.PermiterOfMySquare())
