6d21bbc33bc634381f06acefd81f3d757bda8ce7
Jany Muong <me.roumuong@gmail.com>
2023-07-11 11:42:57 +0300

    specify the default value for the online for each Post object

--- 0x01-challenge/blog/app/models/post.rb
+++ 0x01-challenge/blog/app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ApplicationRecord
   has_many  :comments, dependent: :destroy
+  has_boolean :online, default: true
   validates :title, presence: true, length: {minimum: 5}
 	validates :body,  presence: true
 end
